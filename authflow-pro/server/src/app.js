cfbc631774f8dee7363a11c164aa839c5e214d26
Yash Karande <yashkarande4240@gmail.com>
2026-01-17 17:21:46 +0530

    login controller done

--- authflow-pro/server/src/app.js
+++ authflow-pro/server/src/app.js
@@ -8,4 +8,5 @@ import authRoutes from "./routes/auth.routes.js";
 
 app.use("/api/auth", authRoutes);
 
+
 export default app;
--- authflow-pro/server/src/controllers/auth.controller.js
+++ authflow-pro/server/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import { User } from "../models/User.model.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res) => {
   try {
@@ -33,5 +34,47 @@ export const signup = async (req, res) => {
     });
   }
 
+
+};
+
+export const login = async (req, res) => {
+
+  try {
+    //* 1 Read email & password
+    const {email, password} = req.body;
+  
+    //* 2 Validate email and password
+    if(!email|| !password) return res.status(400).json({status: "failure", message: "both fields are required"});
+  
+    //* 3 Find user by email
+    const user = await User.findOne({email: email});
+  
+    if(!user) return res.status(403).json({status: "failure", message: "invalid credential"});
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+  
+    if(!isPasswordValid) return res.status(403).json({status: "failure", message: "invalid credential"});
+    
+    
+    const payload = {
+      userId: user._id,
+      email: user.email,
+      
+    }
+    
+    const token = await jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRY});
+
+    res.status(200).json({
+      status: "success",
+      message: "login successful",
+      token
+    })
+
+  } catch (error) {
+    return res.status(500).json({
+      status: "failure",
+      message: "Internal server error"
+    });
+  }
 
 };
