22fc82005fc439707ce2924f473273f2fdf077ce
Yash Karande <yashkarande4240@gmail.com>
2026-01-17 15:22:18 +0530

    mongodb setup done

---
+++ authflow-pro/server/src/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/authDB`);
+    console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log("DB connection error: ", error);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
--- authflow-pro/server/src/server.js
+++ authflow-pro/server/src/server.js
@@ -3,9 +3,20 @@ dotenv.config({
   path: "./.env"
 })
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`);
-});
+connectDB()
+.then(() => {
+    app.on("error", (error) => {   // for checking app is working or not
+        console.log(`Error: `, error);
+        throw error;
+      })
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: http://localhost:${PORT}`);
+    })
+  })
+  .catch((err) => {
+  console.log("MONGO db connection failed!!", err);
+  })
