30e74a1405943a3ec1521cf9e6b5b96e9684acd2
Yash Karande <yashkarande4240@gmail.com>
2026-01-18 15:56:58 +0530

    authContext and protected route done

--- authflow-pro/client/src/App.jsx
+++ authflow-pro/client/src/App.jsx
@@ -1,12 +1,25 @@
-import './App.css'
-import Signup from "./pages/Signup"
-function App() {
+import { Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
+function App() {
   return (
-    <>
-      <Signup/>
-    </>
-  )
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
